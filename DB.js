19fd4366ce9c4f29e6728aafede582e23de0ed2d
briiiqtt <briiiqtt@gmail.com>
2022-05-10 18:21:29 +0900

    rest

--- DB.js
+++ DB.js
@@ -1,12 +1,7 @@
-const mysql = require("mysql");
-const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "0000",
-  database: "aig_server_demo",
-});
-
 const DB_NAMESPACE = require("./DB_NAMESPACE");
+const mysql = require("mysql");
+const connection = mysql.createConnection(DB_NAMESPACE.CONN);
+
 const Response = require("./Response");
 
 const query = async function (sql) {
@@ -38,112 +33,13 @@ const iterate = function (argArr) {
   return str.substring(2);
 };
 
-const isValid = function (obj, type, argCnt) {
-  if (type === "array") {
-    return Array.isArray(obj) && argCnt === obj.length;
-  } else if (type === "object") {
-    return !Array.isArray(obj) && argCnt === Object.keys(obj).length;
-  }
-};
-
 const DB = {
-  accounts: {
-    //
-    login: function (obj, res) {
-      if (!isValid(obj, "object", 2)) {
-        new Response(res).badRequest(DB_NAMESPACE.RES_MSG.IS_NOT_VALID);
-        return false;
-      }
-      if (!(obj.ACCOUNT_UUID && obj.PASSWORD)) {
-        new Response(res).badRequest(DB_NAMESPACE.RES_MSG.INSUFFICIENT_VALUE);
-        return false;
-      }
-      let sql = `
-    SELECT
-  	  EMAIL, TEAM
-    FROM
-    	ACCOUNTS
-    WHERE 1=1
-      AND (EMAIL = '${obj.EMAIL}' OR ACCOUNT_UUID = '${obj.ACCOUNT_UUID}')
-      AND PASSWORD = '${obj.PASSWORD}'
-    `;
-      query(sql)
-        .then((r) => {
-          if (r.length == 0) {
-            new Response(res).notFound();
-          } else {
-            new Response(res, r).OK();
-          }
-        })
-        .catch((err) => {
-          console.log(err);
-          new Response(res).internalServerError();
-        });
-    },
-
-    register: function (argArr, res) {
-      if (!isValid(argArr, "array", 4)) {
-        new Response(res).badRequest();
-        return false;
-      }
-      let sql = `
-    INSERT INTO
-      ACCOUNTS(
-        ACCOUNT_UUID,
-        EMAIL,
-        PASSWORD,
-        AUTH,
-        TEAM
-      )
-      VALUES(
-        '${argArr[0]}',
-        '${argArr[1]}',
-        '${argArr[2]}',
-        0,
-        '${argArr[3]}'
-      )
-    `;
-      query(sql)
-        .then((r) => {
-          if (r.affectedRows == 0) {
-            new Response(res).notFound();
-          } else {
-            new Response(res, r).OK();
-          }
-        })
-        .catch((err) => {
-          console.log(err);
-          new Response(res).internalServerError();
-        });
-    },
-
-    changePassword: function (obj, res) {
-      if (!isValid(obj, "object", 3)) {
-        new Response(res).badRequest();
-        return false;
-      }
-      let sql = `
-    UPDATE
-      ACCOUNTS
-    SET
-      PASSWORD = '${obj.NEW_PASSWORD}'
-    WHERE 1=1
-      AND ACCOUNT_UUID = '${obj.ACCOUNT_UUID}'
-      AND PASSWORD = '${obj.CUR_PASSWORD}'
-    `;
-      query(sql)
-        .then((r) => {
-          if (r.affectedRows == 0) {
-            new Response(res).notFound();
-          } else {
-            new Response(res, r).OK();
-          }
-        })
-        .catch((err) => {
-          console.log(err);
-          new Response(res).internalServerError(res);
-        });
-    },
+  ACCOUNTS: {
+    // COLUMNS: ["ACCOUNT_UUID", "_IS_DELETED"],
+    // select() {},
+    // insert() {},
+    // update() {},
+    // delete() {},
   },
 };
 
